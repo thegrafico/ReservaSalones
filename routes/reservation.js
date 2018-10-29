0d24d5b088eea98783b2dba713579d2554458ee9
Raul Pichardo <raul022107@gmail.com>
2018-10-29 16:37:55 -0400

    Checkbox is giving me a value

--- routes/reservation.js
+++ routes/reservation.js
@@ -22,12 +22,40 @@ router.get('/', function(req, res, next) {
 
   if(userName){
     connection.query('SELECT * FROM  building', function (error, results, fields) {
-
       if (error) throw error;
 
       parms.results = results;
       parms.user = userName;
 
+      res.render('reservation', parms);
+    });
+  }else{
+    connection.end();
+    res.redirect('/');
+  }
+});
+
+router.post('/', function(req, res, next) {
+
+  let parms = {layout: 'reservation', title: 'Reservation'};
+  const userName = req.cookies.graph_user_name;
+  const email = req.cookies.graph_user_email;
+
+  //Edif
+  var all 				= req.body.buildingOption[0],
+		 edifG 				= req.body.buildingOption[1],
+		 edifF 	      = req.body.buildingOption[2],
+		 edifC 				= req.body.buildingOption[3];
+
+
+     console.log(all, edifC);
+
+  if(userName){
+    connection.query('SELECT * FROM  building', function (error, results, fields) {
+      if (error) throw error;
+
+      parms.results = results;
+      parms.user = userName;
 
       res.render('reservation', parms);
     });
