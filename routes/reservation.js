0f0a02c5f3c112425c3539788d1ed363b6d817bb
Raul Pichardo <raul022107@gmail.com>
2018-11-05 17:00:48 -0400

    added dinamic hour

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,7 +1,6 @@
-var express = require('express');
-var router = express.Router();
-
-var db = require("../helpers/mysqlConnection").mysql_pool;
+var express = require('express'); //server
+var router = express.Router(); //router
+var db = require("../helpers/mysqlConnection").mysql_pool; //pool connection
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -16,14 +15,25 @@ router.get('/', function(req, res, next) {
     let query = `SELECT * FROM Building Natural Join Room`;
     db.getConnection(function(err, connection) {
       connection.query(query, function (error, results, fields) {
+
+        results.forEach(function(e){
+          e.hourAvailable =  getHour(e.hourAvailable);
+          console.log(e.roomID, e.hourAvailable);
+        });
+
         parms.results = results;
         parms.user = userName;
-        res.render('reservation', parms);
 
+        // results.forEach(function(e){
+        //   letras.push(getHour(e.hourAvailable));
+        // });
+
+        res.render('reservation', parms, );
+
+        //close the connection
         if (error) throw error;
         connection.release();
       });
-
       if(err) throw err;
     });
   }else{
@@ -34,53 +44,86 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
 
   let parms = {layout: 'reservation', title: 'Reservation'};
+  let query = '';
   const userName = req.cookies.graph_user_name;
-  const email = req.cookiesstringRequest.graph_user_email;
 
-  console.log("Result", req.body.buildingOption);
-  var stringRequest = generateString(req.body.buildingOption);
-
+  // console.log("Result", req.body.buildingOption);
+  var stringRequest = generarString(req.body.buildingOption);
   console.log(stringRequest);
 
+  if(stringRequest == "" || stringRequest == undefined){
+    query = `SELECT * FROM Building Natural Join Room`;
+  }else{
+    query = `SELECT * FROM Building NATURAL JOIN Room WHERE ${stringRequest}`;
+  }
+
   if(userName){
-    connection.query('SELECT * FROM  building', function (error, results, fields) {
-      if (error) throw error;
+    db.getConnection(function(err, connection) {
+      connection.query(query, function (error, results, fields) {
+        parms.results = results;
+        parms.user = userName;
 
-      parms.results = results;
-      parms.user = userName;
+        stringRequest = undefined;
+        res.render('reservation', parms);
 
-      res.render('reservation', parms);
+        if (error) throw error;
+        connection.release();
+      });
+      if(err) throw err;
     });
   }else{
-    connection.end();
     res.redirect('/');
   }
 });
 
-function generateString(str){
+function getHour(str){
+
+  var hour = [];
+  for(var i = 0; i < str.length; i++){
+    switch(str[i]){
+      case 'a': hour.push({time: "8:00 - 9:00 am"});
+      break;
+      case 'b': hour.push({time: "9:00 - 10:00 am"});
+      break;
+      case 'c': hour.push({time: "10:00 - 11:00 am"});
+      break;
+      case 'd': hour.push({time: "11:00 - 12:00 midday"});
+      break;
+      case 'e': hour.push({time: "12:00 - 1:00 pm"});
+      break;
+      case 'f': hour.push({time: "1:00 - 2:00 pm"});
+      break;
+      case 'g': hour.push({time: "2:00 - 3:00 pm"});
+      break;
+      case 'h': hour.push({time: "3:00 - 4:00pm"});
+      break;
+      case 'i': hour.push({time: "4:00 - 5:00 pm"});
+      break;
+      default:
+        //do nothing
+    }
+  }
+
+  return hour;
+}
+function generarString(str){
+
   var result = "";
 
-  for(var i = 0; i < str.length ; i++){
+  if(str != undefined){
 
-    if(str[i] == "*"){
-      return "*";
+
+    for(var i = 0; i < str.length ; i++){
+
+        if(i == str.length - 1)
+          result += "edifName = \'" + str[i] + "\'";
+        else
+          result += "edifName = \'" + str[i] + "\' or ";
     }
+    return result;
+  }else{
+    return undefined;
   }
-
-  for(var i = 0; i < str.length ; i++){
-
-    if(str[i] != undefined){
-
-      if(i == str.length - 1){
-        result += str[i];
-      }else{
-        result += str[i] + " and ";
-      }
-    }
-
-  }
-  console.log("RESULTADO: ", result);
-  return result;
 }
 
 module.exports = router;
