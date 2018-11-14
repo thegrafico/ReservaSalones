bd23ea4f25c8c90b28d727e9c33f0ea83b268c84
Carlos Alvarado <cjclonjedi@gmail.com>
2018-11-13 20:54:51 -0400

    Added vars to change the names of the tables

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ var db = require("../helpers/mysqlConnection").mysql_pool;
 /* GET home page. */
 router.get('/', async function(req, res) {
 
-  var user = 'users';
+  var user = 'users';                   //variable to edit the users table of your datebase
+  var pTable = 'professor';             //variable to edit the professor table of your datebase
   const accessToken = await authHelper.getAccessToken(req.cookies, res);
   const userName = req.cookies.graph_user_name;
   const email = req.cookies.graph_user_email;
@@ -25,9 +26,9 @@ router.get('/', async function(req, res) {
 
 
   if(userName){
-    let query =`select profesorEmail` +                              //checks if the user email
-                ` from professor` +                                  //is on the database
-                ` where profesorEmail = '${email}'`;                 //database query
+    let query =`select profEmail` +                              //checks if the user email
+                ` from ${pTable}` +                              //is on the database
+                ` where profEmail = '${email}'`;                 //database query
 
     db.getConnection(function(err, connection) {                     //connects to the database
 
@@ -36,14 +37,14 @@ router.get('/', async function(req, res) {
 
 
       connection.query(query, function (error, results, fields) {    //does the database query
-        //console.log(results);
+        console.log(results);
         var dbEmail = null;                                          //ini the database email to null
 
-        //console.log(results[0].profesorEmail);
-        //console.log(dbEmail);
+        //console.log(results[0].profEmail);
+        console.log(dbEmail);
 
         if (results != "")                                           //checks if the result from the database is empty
-        dbEmail = results[0].profesorEmail;                          //iguala el email del usuario a la variable de dbEmail
+        dbEmail = results[0].profEmail;                          //iguala el email del usuario a la variable de dbEmail
         //console.log(dbEmail);
 
         if (error) throw error;                                      //checks for error
