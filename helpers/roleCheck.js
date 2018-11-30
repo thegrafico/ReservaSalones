260c46226412721345d1035bbac53b313d25db83
Carlos Alvarado <calvarado1458@interbayamon.edu>
2018-11-29 23:50:01 -0400

    Created RoleCheck Code

--- helpers/roleCheck.js
+++ helpers/roleCheck.js
@@ -1,11 +1,37 @@
-var express = require('express')	//requirements for the code
-var router = express.Router()		//requirements for the code
+// Checks the role of the graph_refresh_token
+var db = require("../helpers/mysqlConnection").mysql_pool;
 
-router.get('/', function (req, res) {	//requirements for the code
+function roleCheck(reqRoleID, email, userName){
 
-  const email = req.cookies.graph_user_email;
+  if(userName){
+    let query =`select roleID` +                                     //checks if the user role
+                ` from Users natural join UserRoles` +               //is on the database
+                ` where email = '${email}'`;                         //database query using his email
 
+    db.getConnection(function(err, connection) {                     //connects to the database
+      if (err) throw error;                                            //checks for connection error
 
-})
+      connection.query(query, function (error, results, fields) {    //does the database query
+        console.log(results);
+        var dbRoleID = null;                                          //ini the database role to null
 
-module.exports = router;			//requirements for the code
+        console.log(dbRoleID);
+
+        if (results != "")                                           //checks if the result from the database is empty
+        dbRoleID = results[0].roleID;                                //iguala el roleID de la db a la variable de dbRoleID
+        //console.log(dbEmail);
+
+        if (error) throw error;                                      //checks for error
+
+        console.log(dbRoleID);
+
+        if (dbRoleID == reqRoleID) return true;
+        else{
+          return false;
+        }
+
+        }
+      });
+    });
+  }
+}
