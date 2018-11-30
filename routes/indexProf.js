a021ba258a789cba188febec59485ffe0681851a
Carlos Alvarado <calvarado1458@interbayamon.edu>
2018-11-29 23:57:29 -0400

    Implemented Role Check

--- routes/indexProf.js
+++ routes/indexProf.js
@@ -1,5 +1,6 @@
 var express = require('express')	//requirements for the code
 var router = express.Router()		//requirements for the code
+var roleCheck = require('../helpers/roleCheck'); //path for the roleCheck
 
 
 
@@ -7,9 +8,10 @@ var router = express.Router()		//requirements for the code
 router.get('/', function (req, res) {	//requirements for the code
 
 	const userName = req.cookies.graph_user_name;
+	const email = req.cookies.graph_user_email;
 
-	if(userName){
-  	const userName = req.cookies.graph_user_name; //gets the username from the email
+	if(roleCheck.roleCheck('P', email, userName) == true){
+
   	var layout = './Professor/indexProf';
   	let parms = { title: 'profHome', active: { home: true }, urlReservation: '/reservation', urlProfApp: '/Appointments'};
 
@@ -19,7 +21,7 @@ router.get('/', function (req, res) {	//requirements for the code
 	}
 
 	else{
-		res.redirect('/');
+		res.redirect('/home');
 	}
 })
 
