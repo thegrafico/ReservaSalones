7dd24b31f2bbe491ff728b89d465138c2e3a4094
Raul Pichardo <raul022107@gmail.com>
2018-12-03 14:18:40 -0400

    Master is back, Is look that everything is working nice

--- public/behavior/STUD_APP_HUB_PT2_Beha.js
+++ public/behavior/STUD_APP_HUB_PT2_Beha.js
@@ -8,17 +8,11 @@ $(document).ready(function(){
 
   $(document).ready(function(){
     $('.datepicker').datepicker({
-<<<<<<< HEAD
-
-      //where all options for datepicker are changed
-      disableWeekends: true,
-=======
       //where all options for datepicker are changed
       disableWeekends: true,
       yearRange: 0,
       minDate: 0,
       autoClose: true,
->>>>>>> 836b8d70e90a2dc5a37e69c086160e7a8bebb6ee
       format: 'ddd, mmm, dd,yyyy',
       onSelect:function (dateText, inst)
        {
@@ -48,13 +42,3 @@ $(document).ready(function(){
        }
     });
   });
-// Date Picker Javascript Ends
-
-// This is for the checkbox
-console.log();
-
-// $(document).ready(function(){
-//     $('.check').click(function() {
-//         $('.check').not(this).prop('checked', false);
-//     });
-// });
