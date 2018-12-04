73de8d304d629ac38a087579e8abe20f075cb5b0
Francisco Torres <picki78@gmail.com>
2018-12-04 08:13:59 -0400

    Finished presenting all available appointments

--- routes/profAppointment.js
+++ routes/profAppointment.js
@@ -1,3 +1,10 @@
+/*
+1. does not charge the accept and decline appointments when a button is pressed
+2.accept and decline does not work
+3.CANCEL DOES NOT work
+4. Search for appointments does not work
+*/
+
 var express = require('express')	//requirements for the code
 var router = express.Router()		//requirements for the code
 var dataB = require("../helpers/mysqlConnection").mysql_pool;
@@ -20,27 +27,27 @@ router.get('/', function (req, res) {	//requirements for the code
     connection.query(query_1, function(err, results){
 
       if (results[0] == undefined){
-        console.log("It is undefined1.");
+        // console.log("It is undefined1.");
       }
       else if (results[0] != undefined){
-        console.log ("It is not undefined1.");
+        // console.log ("It is not undefined1.");
         userID = results[0]["userID"];
       }
-      console.log(userID);
-
       let query_2 = `SELECT name, email, start, end, date
                      FROM Users NATURAL JOIN Appointment
                      WHERE profID = '${userID}' AND status = 'Pending'`;
       connection.query(query_2, function(err, results){
 
-        if (results[0] == undefined){
-          console.log("It is undefined2.");
-        }
-        else if (results[0] != undefined){
-          console.log ("It is not undefined2.");
-        }
+        // if (results[0] == undefined){
+        //   console.log("It is undefined2.");
+        // }
+        // else if (results[0] != undefined){
+        //   console.log ("It is not undefined2.");
+        // }
 
         console.log(results);
+
+        parms.appPending = results;
 
         res.render(layName, parms);
         })
@@ -95,11 +102,19 @@ router.post('/', function (req, res) {
       let query_2 = `SELECT userID, start, end, date
                      FROM Appointment
                      WHERE status = 'accept' AND date = '${dateButton}' AND profID = '${userID}'`;
+                     
+      let query_3 = `SELECT name, email, start, end, date
+                     FROM Users NATURAL JOIN Appointment
+                     WHERE profID = '${userID}' AND status = 'Pending'`;
       console.log(query_2);
 
-
+      connection.query(query_3, function (err, results){
+        parms.appPending = results;
 
         res.render(layName, parms);
+
+
+      })
     })
   })
 
