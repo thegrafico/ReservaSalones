53a870c4a2f09b3acb75d8ea8eee2d7d2df2daf5
Carlos Alvarado <cjclonjedi@gmail.com>
2018-12-05 17:34:41 -0400

    Connected to Lab Server

--- helpers/mysqlConnection.js
+++ helpers/mysqlConnection.js
@@ -11,10 +11,10 @@ config = {
       connectionLimit : 15,
 
       //establece el route basico donde se ouede accessar
-      host     : 'localhost',          //THIS IS THE SAME FOR YOUR
+      host     : '192.168.1.11',          //THIS IS THE SAME FOR YOUR
       user     : 'root',               //THIS IS THE SAME FOR YOUR
-      password : 'radames121',         //HERE GO YOUR PASSWORD TO ENTER IN YOUR DB
-      database : 'Inter Reservation'   //HERE GO THE DATABASE THAT WE ARE GONNA USED
+      password : 'robolab',         //HERE GO YOUR PASSWORD TO ENTER IN YOUR DB
+      database : 'InterReservations'   //HERE GO THE DATABASE THAT WE ARE GONNA USED
     })
 };
 
