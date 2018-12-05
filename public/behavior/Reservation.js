fef9d311b5814949d5d777e0866f2e49db6b7c8a
Noah R. Almeda Sanchez <noahalmeda@gmail.com>
2018-12-05 09:33:08 -0400

    Finished the Admin pages, added Carlos Back End changes

--- public/behavior/Reservation.js
+++ public/behavior/Reservation.js
@@ -1,18 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.dropdown-trigger');
-//   var instances = M.Dropdown.init(elems, options);
-// });
-
-// Or with jQuery
-
 $('.dropdown-trigger').dropdown();
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.datepicker');
-//   var instances = M.Datepicker.init(elems, options);
-// });
-
-// Or with jQuery
 
 $(document).ready(function(){
   $('.datepicker').datepicker({
@@ -51,40 +37,18 @@ $(document).ready(function(){
   });
 });
 
-  // document.addEventListener('DOMContentLoaded', function() {
-  //   var elems = document.querySelectorAll('select');
-  //   var instances = M.FormSelect.init(elems, options);
-  // });
+$(document).ready(function(){
+  $('select').formSelect();
+});
 
-  // Or with jQuery
+$(document).ready(function(){
+  $('.timepicker').timepicker(
+  {
+    defaultTime:12,
+  }
+  );
+});
 
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
-
-  // document.addEventListener('DOMContentLoaded', function() {
-  //     var elems = document.querySelectorAll('.timepicker');
-  //     var instances = M.Timepicker.init(elems, options);
-  //   });
-
-    // Or with jQuery
-
-    $(document).ready(function(){
-      $('.timepicker').timepicker(
-      {
-        defaultTime:12,
-      }
-      );
-    });
-
-
-    // document.addEventListener('DOMContentLoaded', function() {
-    //    var elems = document.querySelectorAll('.collapsible');
-    //    var instances = M.Collapsible.init(elems, options);
-    //  });
-
-     // Or with jQuery
-
-     $(document).ready(function(){
-       $('.collapsible').collapsible();
-     });
+$(document).ready(function(){
+ $('.collapsible').collapsible();
+});
