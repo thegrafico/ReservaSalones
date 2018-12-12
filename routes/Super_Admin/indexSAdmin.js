d2bac204c1fa3e8aa9447c6c235e0586c0485e2d
Carlos Alvarado <calvarado1458@interbayamon.edu>
2018-12-12 05:13:35 -0400

    Implemented Group By with Accepted, Decline and Pending

--- routes/Super_Admin/indexSAdmin.js
+++ routes/Super_Admin/indexSAdmin.js
@@ -1,6 +1,7 @@
 var express = require('express')											 //requirements for the code
 var router = express.Router()													 //requirements for the code
 var roleCheckHelper = require('../../helpers/roleCheck'); //path for the roleCheck
+var db = require("../../helpers/mysqlConnection").mysql_pool;
 
 router.get('/', function (req, res) {	//requirements for the code
 
@@ -14,8 +15,29 @@ router.get('/', function (req, res) {	//requirements for the code
 			var layout = './Super_Admin/indexSAdmin'; /* ==== Missing! ==== */											/* === Missing! === */
 		  let parms = { title: 'adminRoom	', active: { home: true }, urlManageRoom: '/superAdminHome/manageRoom', urlManageRole: '/superAdminHome/manageRole'};
 
-		 	parms.user = userName;
-			res.render(layout, parms);
+			let getStats = `Select *
+			FROM (Select count(status) count, status from ResDecline natural join
+			(select distinct(roomID) from Rooms natural join  (select userID, deptID from Users natural join DeptManagers) as DUsers where userID = 1) UReservations
+			group by status) ResDecline2
+			union all
+			(select count(status),status from Reservation natural join
+			(select distinct(roomID) from Rooms natural join  (select userID, deptID from Users natural join DeptManagers) as DUsers where userID = 1) UReservations
+			group by status)
+			order by status;`
+
+			db.getConnection(function (err, connection) {
+				if (err) throw error;
+				connection.query (getStats, function (err, results, fields){
+
+					parms.accepted = results[0].count;
+					parms.decline	 = results[1].count;
+					parms.pending	 = results[2].count;
+
+					parms.user = userName;
+					res.render(layout, parms);
+				})
+				connection.release();
+			})
 		}
 
 		else{
