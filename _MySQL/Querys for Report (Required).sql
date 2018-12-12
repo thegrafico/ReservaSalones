5c2c6f624579217c4d2e5cf1c4d8625fcd79028e
Carlos Alvarado <cjclonjedi@gmail.com>
2018-12-12 15:27:52 -0400

    Change query to new table

--- _MySQL/Querys for Report (Required).sql
+++ _MySQL/Querys for Report (Required).sql
@@ -4,7 +4,7 @@ insert into Reservation (userID, start, end, date, roomID, status)
 SELECT *
 FROM (Select 2, '21:00:00','22:00:00', 'Mon, Dec, 17,2018', 'G240', 'Pending' ) as NRoomHours
 	  WHERE not exists (Select * from (select roomID, start, end, day date, description
-	  from RoomHours union all Select roomID, start, end, date, description from Reservation where status = 'Accepted') AllReservation
+	  from RoomHours union all Select roomID, start, end, date, description from Reservation_Status where status = 'Accepted') AllReservation
 where (end > '21:00:00' and start < '22:00:00') and roomID = 'G240' and (date = 'Mon, Dec, 17,2018' or date = 'Mon' or date = 'all'));
                        
 /* Count Query */
@@ -18,7 +18,7 @@ where status = 'Pending';
 /*Group Query*/
 
 Select *
-FROM (Select count(status) count, status from ResDecline natural join (select distinct(roomID) from Rooms) Rooms
+FROM (Select count(status) count, status from Reservation_Status natural join (select distinct(roomID) from Rooms) Rooms
 group by status) ResDecline2
 union all
 (select count(status),status from Reservation natural join (select distinct(roomID) from Rooms) Rooms
